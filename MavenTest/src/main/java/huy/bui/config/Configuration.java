b06eb2c7808136bd0c62c15289f209075b380c4b
brown11188 <buiquanghuy.it@gmail.com>
2015-08-14 16:54:31 +0700

    configuration

---
+++ MavenTest/src/main/java/huy/bui/config/Configuration.java
@@ -0,0 +1,46 @@
+package huy.bui.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import huy.bui.dao.EmployeeJDBCTemplate;
+
+@org.springframework.context.annotation.Configuration
+@ComponentScan("huy.bui")
+@EnableWebMvc
+public class Configuration extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/employee");
+		dataSource.setUsername("root");
+		dataSource.setPassword("123456789");
+		return dataSource;
+	}
+
+	@Bean(name = "employeeDAO")
+	public EmployeeJDBCTemplate getEmployeeJDBCTemplate() {
+		EmployeeJDBCTemplate employeeDAO = new EmployeeJDBCTemplate();
+
+		return employeeDAO;
+	}
+
+}
